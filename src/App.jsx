d469e1966a726444f9405c216b5c4eee0b4d3ef4
G4tes <50048258+G4tes@users.noreply.github.com>
2023-07-22 16:31:37 +0700

    nambahin router biar keceeee

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CartUniqlo from "./carts/page";
 import AppLocation from "./maps/page";
+import Home from "./pages/Home";
 import ProductsByID from "./products/id/page";
 
 function App() {
   return (
     <>
-      {/* maps */}
-      {/* <AppLocation /> */}
-
-      {/* carts */}
-      <CartUniqlo />
-
-      {/* <ProductsByID /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products/:id" element={<ProductsByID />} />
+          <Route path="/cart" element={<CartUniqlo />} />
+          <Route path="/maps" element={<AppLocation />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
