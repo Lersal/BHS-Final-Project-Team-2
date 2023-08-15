c3dfb6027aeb6c290ab17ec611651958fe5d780b
G4tes <50048258+G4tes@users.noreply.github.com>
2023-08-15 16:09:43 +0700

    add provider redux

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,27 @@ import Home from "./pages/Home";
 import ProductsByID from "./products/id/page";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import { Provider } from "react-redux";
+import { persistor, store } from "./stores";
+import { PersistGate } from "redux-persist/integration/react";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products/:id" element={<ProductsByID />} />
-          <Route path="/cart" element={<CartUniqlo />} />
-          <Route path="/maps" element={<AppLocation />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/maps" element={<AppLocation />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/products/:id" element={<ProductsByID />} />
+              <Route path="/cart" element={<CartUniqlo />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
     </>
   );
 }
