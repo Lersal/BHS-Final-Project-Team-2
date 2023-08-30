9c3b2f45ed4963723b107d85ad565a388b8f6bdb
aldiipr <aldipriono@gmail.com>
2023-08-30 20:52:21 +0700

    Edit layout products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
+import Footer from "../components/Footer";
 
 export default function Products(props) {
   const [dataProducts, setDataProducts] = useState([]);
@@ -19,26 +20,35 @@ export default function Products(props) {
 
   return (
     <section>
-      <div className="p-10 m-auto max-w-5xl grid grid-cols-4">
+      <div className="px-10 py-5 m-auto max-w-5xl grid grid-cols-4 gap-5">
         {dataProducts.map((element) => (
-          <div className="mx-2 my-2 px-2 py-2" key={element.id}>
-            <img src={element.image} />
-            <div className="flex justify-between text-zinc-400 text-sm font-bold">
-              <span>{element.category}</span>
-              <span>{element.tb}</span>
+          <div className="mx-2 my-2 px-1 rounded shadow" key={element.id}>
+            <div className="flex justify-center">
+              <img src={element.image} className="h-44 w-auto " />
             </div>
-            <h1 className="text-md font-bold text-zinc-600">{element.title}</h1>
-            <div>
-              <span className="text-lg font-bold text-zinc-400">
-                {Number(element.price).toLocaleString("id-ID", {
-                  currency: "IDR",
-                  style: "currency",
-                })}
-              </span>
+            <div className="grid  row-span-full border-t-2 px-1 ">
+              <div className="flex justify-between text-zinc-400 text-sm font-bold mt-2 ">
+                  <span>{element.category}</span>
+                  <span>{element.tb}</span>
+              </div>
+              <div className="h-24 w-auto">
+                <h1 className="text-md font-bold text-zinc-600 my-2 font-sans ">{element.title}</h1>
+              </div>
+              
+              <div className="my-3"> 
+                <span className="text-lg font-bold font-sans text-zinc-800 text-center">
+                  {Number(element.price).toLocaleString("id-ID", {
+                    currency: "IDR",
+                    style: "currency",
+                  })}
+                </span>
+              </div>
             </div>
+           
           </div>
         ))}
       </div>
+      <Footer/>
     </section>
   );
 }
